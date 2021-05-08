b56fbccd4b55c6938bfe1f44139883e87e0c2da4
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-05-09 01:10:11 +0200

    Update Main.cpp

--- gamecube/source/Main.cpp
+++ gamecube/source/Main.cpp
@@ -1,1 +1,46 @@
+// 
+// Main.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include <gccore.h>
+#include <fat.h>
+#include <grrlib.h>
+#include <asndlib.h>
+
+#include "font_ttf.h"
+
+#define GRRLIB_BLACK 0x000000FF
+#define GRRLIB_WHITE 0xFFFFFFFF
+
+const int screenWidth = 640;
+const int screenHeight = 480;
+
+int main(void)
+{
+    ASND_Init();
+    PAD_Init();
+    GRRLIB_Init();
+
+    GRRLIB_ttfFont *font = GRRLIB_LoadTTF(font_ttf, font_ttf_size);
+       
+    for (;;)   
+    {
+        GRRLIB_FillScreen(GRRLIB_BLACK); 
+        PAD_ScanPads();
+        
+        if(PAD_ButtonsDown(0) & PAD_BUTTON_START)
+        {
+            break;
+        }                    
+
+        GRRLIB_PrintfTTF(screenWidth/2 - 146, screenHeight/2, font, "SchumakerTeam", 64, GRRLIB_WHITE);
+        GRRLIB_Render();        
+    }
+
+    GRRLIB_FreeTTF(font);
+    GRRLIB_Exit();
+    return 0;
+}
 
