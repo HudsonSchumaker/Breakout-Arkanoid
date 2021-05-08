9eb4eca8b885c7d14994f8b48ead0524f2f9be9c
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-09 01:27:43 +0200

    added Canvas.h

---
+++ gamecube/source/Canvas.h
@@ -0,0 +1,16 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+class Canvas
+{
+    private:
+        int w;
+        int h;
+
+    public:
+        void setWidth(int w) { this->w = w; };
+	    int getWidth() const { return w; };
+
+	    void setHeight(int h) { this->h = h; };
+	    int getHeight() const { return h; };
+};
+#endif /* CANVAS_H */
