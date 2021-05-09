a2c9a70406b632b3d1e51e799b64293cdcfb3869
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-10 00:02:50 +0200

    Paddle header changes

---
+++ gamecube/source/Paddle.h
@@ -0,0 +1,25 @@
+//
+// Paddle.h
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#ifndef PADDLE_H
+#define PADDLE_H
+
+#include <grrlib.h>
+#include "Sprite.h"
+
+class Paddle : public Sprite {
+public:
+    Paddle() : Sprite() {};
+    Paddle(int x, int y) : Sprite(x, y) {};
+    Paddle(int x, int y, int w, int h) : Sprite(x, y, w, h) {};
+    void move(int dx);
+    void draw();
+    void draw(float d);
+    
+private:
+  
+};
+#endif /* PADDLE_H */
