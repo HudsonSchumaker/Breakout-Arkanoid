bf43eb9cfa3d3856791668bf86e0cfecdba14c17
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-10 00:02:38 +0200

    Sprite changes

--- gamecube/source/Sprite.h
+++ gamecube/source/Sprite.h
@@ -11,29 +11,29 @@
 #include "Rectangle.h"
 
 class Sprite {
-    protected:
+protected:
         int x, y, w, h;
 		
-    public:
-		Sprite();
-		Sprite(int w, int h);
-	    Sprite(int x, int y, int w, int h);
-		Rectangle getBounds();
-		GRRLIB_texImg *sprite;
-		void setTexture(GRRLIB_texImg *spr) {
-			sprite = spr; w = sprite->w; h = sprite->h;
-		};
+public:
+	Sprite();
+	Sprite(int x, int y);
+    Sprite(int x, int y, int w, int h);
+	Rectangle getBounds();
+	GRRLIB_texImg *sprite;
+	void setTexture(GRRLIB_texImg *spr) {
+		sprite = spr; w = sprite->w; h = sprite->h;
+	};
 		
-        void setX(int x) { this->x = x; };
-	    int getX() const { return x; };
+	int getX() const { return x; };
+	void setX(int x) { this->x = x; };
 
-	    void setY(int y) { this->y = y; };
-	    int getY() const { return y; };
+    int getY() const { return y; };
+    void setY(int y) { this->y = y; };
 
-	    void setWidth(int w) { this->w = w; };
-	    int getWidth() const { return w; };
+    int getWidth() const { return w; };
+    void setWidth(int w) { this->w = w; };
 
-	    void setHeight(int h) { this->h = h; };
-	    int getHeight() const { return h; };
+	int getHeight() const { return h; };
+    void setHeight(int h) { this->h = h; };
 };
 #endif /* SPRITE_H */
