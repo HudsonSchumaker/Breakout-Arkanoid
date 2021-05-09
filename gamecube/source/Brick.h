e9a54bc95d2630629b33989133ea4682e0da99c4
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-10 00:02:23 +0200

    Brick header

--- gamecube/source/Brick.h
+++ gamecube/source/Brick.h
@@ -13,7 +13,7 @@
 class Brick : public Sprite {
 public:
     Brick() : Sprite() {};
-    Brick(int w, int h) : Sprite(w, h) {};
+    Brick(int x, int y) : Sprite(x, y) {};
     Brick(int x, int y, int w, int h) : Sprite(x, y, w, h) {};
     void draw();
     void draw(float d);
