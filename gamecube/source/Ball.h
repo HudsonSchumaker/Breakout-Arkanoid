c3f18e89bdfda00b256ce19f5a4aed22a887b2a2
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-11 23:03:27 +0200

    ball

--- gamecube/source/Ball.h
+++ gamecube/source/Ball.h
@@ -12,9 +12,17 @@ public:
     Ball() : Sprite() {};
     Ball(int x, int y) : Sprite(x, y) {};
     Ball(int x, int y, int w, int h) : Sprite(x, y, w, h) {};
+    void move();
+    void reset(int x, int y);
     void draw() override;
     void draw(float d);
 
-private:
+    int getDX() const { return dx; };
+	void setDX(int dx) { this->dx = dx; };
 
+    int getDY() const { return dy; };
+    void setDY(int dy) { this->dy = dy; };
+
+private:
+    int dx, dy;
 };
