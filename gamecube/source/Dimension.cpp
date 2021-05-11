56ea485ead58f3679f030a9a2351b41324cb3259
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-11 20:49:35 +0200

    Changes Dimension

--- gamecube/source/Dimension.cpp
+++ gamecube/source/Dimension.cpp
@@ -6,15 +6,8 @@
 
 #include "Dimension.h"
 
-Dimension::Dimension() {
-	this->w = 0;
-	this->h = 0;
-}
-
-Dimension::Dimension(int w, int h) {
-	this->w = w;
-	this->h = h;
-}
+Dimension::Dimension() : w(0), h(0) {}
+Dimension::Dimension(int w, int h) : w(w), h(h) {}
 
 void Dimension::setDimension(Dimension d) {
 	this->w = d.getWidth();
