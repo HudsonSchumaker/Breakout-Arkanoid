a1238bca51c1ee5e8006197fd977458c62f27208
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-11 20:47:02 +0200

    Point class changes

--- gamecube/source/Point.cpp
+++ gamecube/source/Point.cpp
@@ -7,15 +7,8 @@
 
 #include "Point.h"
 
-Point::Point() {
-    this->x = 0;
-    this->y = 0;
-}
-
-Point::Point(int x, int y) {
-    this->x = x;
-    this->y = y;
-}
+Point::Point() : x(0), y(0) {}
+Point::Point(int x, int y) : x(x), y(y) {}
 
 void Point::move(int x, int y) {
     this->x = x;
