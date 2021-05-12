35b62a75a2dcd46c24d188fe6d1637c848315e21
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-12 19:56:41 +0200

    Ball changes.

--- gamecube/source/Ball.h
+++ gamecube/source/Ball.h
@@ -9,9 +9,10 @@
 
 class Ball final: public Sprite {
 public:
-    Ball() : Sprite(), dx(1), dy(-1) {};
-    Ball(int x, int y) : Sprite(x, y), dx(1), dy(-1) {};
-    Ball(int x, int y, int w, int h) : Sprite(x, y, w, h), dx(1), dy(-1) {};
+    Ball() : Sprite(), dx(1), dy(-1), s(1) {};
+    Ball(int x, int y) : Sprite(x, y), dx(1), dy(-1), s(0) {};
+    Ball(int x, int y, int w, int h) : Sprite(x, y, w, h), dx(1), dy(-1), s(0) {};
+    
     void move();
     void reset(int x, int y);
     void draw() override;
@@ -19,10 +20,10 @@ public:
 
     int getDX() const { return dx; };
 	void setDX(int dx) { this->dx = dx; };
-
     int getDY() const { return dy; };
     void setDY(int dy) { this->dy = dy; };
 
 private:
     int dx, dy;
+    int s;
 };
