1d1f9c37d7d57fbf43e523b774ec12153ebf667c
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-14 22:04:36 +0200

    SDL2

--- swicth/source/Brick.cpp
+++ swicth/source/Brick.cpp
@@ -7,22 +7,13 @@
 #include "Brick.hpp"
 #include "Color.hpp"
 
-void Brick::draw() {
-    GRRLIB_DrawImg(x, y, sprite, 0, 1, 1, Color::getWhite());
+void Brick::draw(SDL_Renderer* renderer) {
+    SDL_Rect rect = { x, y, w, h };
+    SDL_RenderCopy(renderer, sprite, NULL, &rect);
 }
 
 void Brick::draw(float d) {
-    GRRLIB_DrawImg(x, y, sprite, d, 1, 1, Color::getWhite());
+    //GRRLIB_DrawImg(x, y, sprite, d, 1, 1, Color::getWhite());
 }
 
-/*
-GRRLIB  GRRLIB_DrawImg method params
-xpos	Specifies the x-coordinate of the upper-left corner.
-ypos	Specifies the y-coordinate of the upper-left corner.
-tex	    The texture to draw.
-degrees	Angle of rotation.
-scaleX	Specifies the x-coordinate scale. -1 could be used for flipping the texture horizontally.
-scaleY	Specifies the y-coordinate scale. -1 could be used for flipping the texture vertically.
-color	Color in RGBA format.
-*/
 
