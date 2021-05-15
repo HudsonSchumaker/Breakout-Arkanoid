cca22772a938855eb51c9f90008095f01e173a27
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-15 20:44:54 +0200

    NGC-Changes

--- gamecube/source/Ball.cpp
+++ gamecube/source/Ball.cpp
@@ -23,3 +23,14 @@ void Ball::draw() {
 void Ball::draw(float d) {
     GRRLIB_DrawImg(x, y, sprite, d, 1, 1, Color::getWhite());
 }
+
+/*
+GRRLIB  GRRLIB_DrawImg method params
+xpos	Specifies the x-coordinate of the upper-left corner.
+ypos	Specifies the y-coordinate of the upper-left corner.
+tex	    The texture to draw.
+degrees	Angle of rotation.
+scaleX	Specifies the x-coordinate scale. -1 could be used for flipping the texture horizontally.
+scaleY	Specifies the y-coordinate scale. -1 could be used for flipping the texture vertically.
+color	Color in RGBA format.
+*/
