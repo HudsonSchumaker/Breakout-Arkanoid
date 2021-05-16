0c6988d6fd07211c37ea3c3259df27c53148393b
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-16 23:16:04 +0200

    NGC-leve1 changes

--- gamecube/source/Level1.cpp
+++ gamecube/source/Level1.cpp
@@ -5,6 +5,12 @@
 //
 
 #include "Level1.hpp"
+#include "Color.hpp"
+
+#include "ball_png.h"
+#include "brick_png.h"
+#include "paddle_png.h"
+#include "background_png.h"
 
 Level1::Level1() {
     load();
@@ -15,7 +21,12 @@ Level1::~Level1() {
 }
 
 void Level1::loop() {
-
+    for(;;) {
+        input(); 
+        move();
+        collision();
+        render();
+    }
 }
 
 void Level1::input() {
@@ -31,7 +42,19 @@ void Level1::collision() {
 }
 
 void Level1::render() {
+    GRRLIB_FillScreen(Color::getBlack()); 
+    GRRLIB_DrawImg(128, 0, back_img, 0, 1, 1, Color::getWhite());
 
+    for (unsigned int i = 0; i < sizeof bricks; i++) {
+        if (!bricks[i].isDestroyed()) {
+            bricks[i].draw();
+        }
+    }
+
+    ball.draw();
+    paddle.draw();
+    
+    GRRLIB_Render();           
 }
 
 void Level1::load() {
@@ -39,6 +62,20 @@ void Level1::load() {
     paddle_img = GRRLIB_LoadTexture(paddle_png);
     ball_img = GRRLIB_LoadTexture(ball_png);
     back_img = GRRLIB_LoadTexture(background_png);
+
+    int b = 0;
+    for (int l = 0; l < 2; l++) {
+        for (int c = 0; c < 12; c++) {
+            bricks[b] = Brick(c * brick_img->w + 128, l * brick_img->h + 64);
+            bricks[b].setTexture(brick_img);
+            b++;
+        }
+    }
+
+    paddle = Paddle(192, 462);
+    ball.setS(3);
+
+    ball = Ball(200, 440);
 }
 
 void Level1::unload() {
