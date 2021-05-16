3cd647bb5d62539a54a55f9f6006953bdde0d22c
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-16 22:51:52 +0200

    NGC-level1

--- gamecube/source/Level1.hpp
+++ gamecube/source/Level1.hpp
@@ -6,16 +6,23 @@
 
 #pragma once
 #include "Canvas.hpp"
+#include <grrlib.h>
 
 class Level1 final : public Canvas {
 public:
+
+    GRRLIB_texImg* brick_img;
+    GRRLIB_texImg* paddle_img;
+    GRRLIB_texImg* ball_img;
+    GRRLIB_texImg* back_img;
+
     Level1();
     ~Level1();
 
     void loop() override;
     void input() override; 
     void move() override;
-    void collision() override;
+    void collision() override;4
     void render() override;
     void load() override;
     void unload() override;
