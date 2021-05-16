360cb0ba135c45ff7995998b789037afe3f8a763
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-16 22:51:41 +0200

    NGC-changes

--- gamecube/source/Level1.cpp
+++ gamecube/source/Level1.cpp
@@ -6,8 +6,13 @@
 
 #include "Level1.hpp"
 
-Level1::Level1() {}
-Level1::~Level1() {}
+Level1::Level1() {
+    load();
+}
+
+Level1::~Level1() {
+    unload();
+}
 
 void Level1::loop() {
 
@@ -30,9 +35,15 @@ void Level1::render() {
 }
 
 void Level1::load() {
-
+    brick_img = GRRLIB_LoadTexture(brick_png);
+    paddle_img = GRRLIB_LoadTexture(paddle_png);
+    ball_img = GRRLIB_LoadTexture(ball_png);
+    back_img = GRRLIB_LoadTexture(background_png);
 }
 
 void Level1::unload() {
-
+    GRRLIB_FreeTexture(brick_img);
+    GRRLIB_FreeTexture(paddle_img);
+    GRRLIB_FreeTexture(ball_img);
+    GRRLIB_FreeTexture(back_img);
 }
