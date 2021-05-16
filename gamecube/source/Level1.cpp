ed4ab83923008a53ffa1b34bb40abc0c7982b155
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-16 19:34:06 +0200

    NCG-changes

---
+++ gamecube/source/Level1.cpp
@@ -0,0 +1,38 @@
+//
+// Level1.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Level1.hpp"
+
+Level1::Level1() {}
+Level1::~Level1() {}
+
+void Level1::loop() {
+
+}
+
+void Level1::input() {
+
+}
+
+void Level1::move() {
+
+}
+
+void Level1::collision() {
+
+}
+
+void Level1::render() {
+
+}
+
+void Level1::load() {
+
+}
+
+void Level1::unload() {
+
+}
