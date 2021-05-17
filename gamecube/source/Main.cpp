50e6a7257f1d42344216b5f99dc4bdeb7f7faa79
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-17 22:09:06 +0200

    NGC-Main.cpp changes

--- gamecube/source/Main.cpp
+++ gamecube/source/Main.cpp
@@ -5,36 +5,15 @@
 //
 
 #include <fat.h>
-#include <thread>  
-#include <fstream>
 #include <gccore.h>
 #include <grrlib.h>
 #include <asndlib.h>
 #include <mp3player.h>
 
-#include "Brick.hpp"
-#include "Paddle.hpp"
-#include "Ball.hpp"
-#include "Color.hpp"
-#include "Point.hpp"
-
-#include "font_ttf.h"
-#include "brick_png.h"
-#include "paddle_png.h"
-#include "ball_png.h"
-#include "background_png.h"
-
-#include "beep_mp3.h"
 #include "Level1.hpp"
 
-const int screenWidth = 640;
-const int screenHeight = 480;
 void ini();
-void input();
-void collision();
-void render();
 void end();
-void play();
 
 int main(void) {
     ini();
@@ -44,7 +23,6 @@ int main(void) {
     end();
     return 0;
 }
-
 
 void ini() {
     ASND_Init();
@@ -58,6 +36,3 @@ void end() {
     GRRLIB_Exit();
 }
 
-void play() {
-    MP3Player_PlayBuffer(beep_mp3, beep_mp3_size, NULL);
-}
