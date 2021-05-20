502a2311987bbe17938947b357757df2626d3ad9
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-20 18:37:06 +0200

    NS-Main changes

--- swicth/source/Main.cpp
+++ swicth/source/Main.cpp
@@ -22,6 +22,7 @@
 #include "Ball.hpp"
 #include "Color.hpp"
 #include "Point.hpp"
+#include "Level1.hpp"
 
 // some switch buttons
 #define JOY_A     0
@@ -39,194 +40,19 @@ const int screenWidth = 800;
 const int screenHeight = 450;
 
 void ini();
-void input(SDL_Event& e);
-void collision();
-void render();
 void end();
-void play();
-
-SDL_Surface* paddle_surf;
-SDL_Surface* brick_surf;
-SDL_Surface* ball_surf;
-SDL_Surface* background_surf;
-
-SDL_Texture* paddleSprite;
-SDL_Texture* brickSprite; //= SDL_CreateTextureFromSurface(renderer, lavaSprite_surf);
-SDL_Texture* ballSprite; //= SDL_CreateTextureFromSurface(renderer, platformSprite_surf);
-SDL_Texture* backgroundSprite; // = SDL_CreateTextureFromSurface(renderer, coinSprite_surf);
-
-Mix_Chunk* fxLaunch;
-
-Paddle paddle(192, 432);
-Brick bricks [24];    
-Ball ball(200, 425);
 
 SDL_Renderer* renderer;
 
 int main(void) {
     ini();
     
-    ball.setS(1);
-    
-    paddle_surf = IMG_Load("resources/paddle.png");
-    brick_surf = IMG_Load("resources/brick.png");
-    ball_surf = IMG_Load("resources/ball.png");
-    background_surf = IMG_Load("resources/backgrund.png");
-
-    fxLaunch = Mix_LoadWAV("resources/beep.wav"); 
-
-    paddleSprite = SDL_CreateTextureFromSurface(renderer, paddle_surf);
-    brickSprite = SDL_CreateTextureFromSurface(renderer, brick_surf);
-    ballSprite = SDL_CreateTextureFromSurface(renderer, ball_surf);
-    backgroundSprite = SDL_CreateTextureFromSurface(renderer, background_surf);
-    
-    paddle.setTexture(paddleSprite);
-    ball.setTexture(ballSprite);
-
-    int b = 0;
-    SDL_Point size;
-    SDL_QueryTexture(brickSprite, NULL, NULL, &size.x, &size.y);
-    for (int l = 0; l < 2; l++) {
-        for (int c = 0; c < 12; c++) {
-            bricks[b] = Brick(c * size.x + 128, l * size.y + 64);
-            bricks[b].setTexture(brickSprite);
-            b++;
-        }
-    }
-
-    for (;;) {
-        SDL_Event e;
-        input(e);
-        ball.move();
-        collision();
-        render(); 
-    }
-
+    Level1* lv1 = new Level1(renderer);
+    lv1->loop();
     end();
     return 0;
 }
 
-void input(SDL_Event& e) {
-    int dx = 0;
-    while(SDL_PollEvent(&e)) {
-        switch(e.type) {
-            case SDL_QUIT: {
-                bool quit = true;
-            } break;
-            case SDL_JOYAXISMOTION: {
-                dx = e.jaxis.value;
-            } break;
-        }
-    }
-
-    if (dx > 0) {
-        paddle.move(4);
-        return;
-    } 
-    if (dx < 0) {
-        paddle.move(-4);
-        return;
-    }
-}
-
-void collision() {
-    if (ball.getBounds().getY() > screenHeight) {
-        exit(1);
-    }
-
-    for (unsigned int i = 0, j = 0; i < sizeof bricks; i++) {
-        if (bricks[i].isDestroyed()) {
-            j++;
-        }
-        if (j == 16) {
-            //message = "Victory";
-            //GRRLIB_PrintfTTF(screenWidth/2 - 146, screenHeight/2, font,"Victory", 64, Color::getBlack());
-            
-            // exit(0);
-        }
-    }
-
-    if ((ball.getBounds()).intersects(paddle.getBounds())) {
-        int paddleLPos = paddle.getBounds().getX();
-        int ballLPos = ball.getBounds().getX();
-        int first  = paddleLPos + 8;
-        int second = paddleLPos + 16;
-        int third  = paddleLPos + 24;
-        int fourth = paddleLPos + 32;
-
-        if (ballLPos < first) {
-            ball.setDX(-1);
-            ball.setDY(-1);
-        }
-        if (ballLPos >= first && ballLPos < second) {
-            ball.setDX(-1);
-            ball.setDY(-1 * ball.getDY());
-        }
-        if (ballLPos >= second && ballLPos < third) {
-            ball.setDX(0);
-            ball.setDY(-1);
-        }
-        if (ballLPos >= third && ballLPos < fourth) {
-            ball.setDX(1);
-            ball.setDY(-1 * ball.getDY());
-        }
-        if (ballLPos > fourth) {
-            ball.setDX(1);
-            ball.setDY(-1);
-        }
-    }
-
-    for (unsigned int i = 0; i < sizeof bricks; i++) {
-        if ((ball.getBounds()).intersects(bricks[i].getBounds())) {
-
-            if(bricks[i].isDestroyed()) {
-                return;
-            }
-
-            bricks[i].setDestroyed(true);
-            
-            int ballLeft   = ball.getBounds().getX();
-            int ballHeight = ball.getBounds().getHeight();
-            int ballWidth  = ball.getBounds().getWidth();
-            int ballTop    = ball.getBounds().getY();
-
-            Point pointRight(ballLeft + ballWidth + 1, ballTop);
-            Point pointLeft(ballLeft - 1, ballTop);
-            Point pointTop(ballLeft, ballTop - 1);
-            Point pointBottom(ballLeft, ballTop + ballHeight + 1);
-
-            if (bricks[i].getBounds().contains(pointRight)) {
-                ball.setDX(-1);
-            }
-            else if (bricks[i].getBounds().contains(pointLeft)) {
-                ball.setDX(1);
-            }
-            if (bricks[i].getBounds().contains(pointTop)) {
-                ball.setDY(1);
-            }
-            else if (bricks[i].getBounds().contains(pointBottom)) {
-                ball.setDY(-1);
-            }
-
-            //std::thread(play);
-            //PlayOgg(beep_ogg, beep_ogg_size, 0, OGG_ONE_TIME);
-        }
-    }
-}
-
-void render() {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    for (unsigned int i = 0; i < sizeof bricks; i++) {
-        if (!bricks[i].isDestroyed()) {
-            bricks[i].draw(renderer, 45.0);
-        }
-    }
-    paddle.draw(renderer);
-    ball.draw(renderer);
-    //SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);     
-}
 
 void ini() {
     romfsInit();
@@ -251,6 +77,9 @@ void ini() {
     SDL_InitSubSystem(SDL_INIT_JOYSTICK);
     SDL_JoystickEventState(SDL_ENABLE);
     SDL_JoystickOpen(0);
+    SDL_JoystickOpen(1);
+    SDL_JoystickOpen(2);
+    SDL_JoystickOpen(3);
 }
 
 void end() {
@@ -261,6 +90,3 @@ void end() {
     romfsExit();
 }
 
-void play() {
-    Mix_PlayChannel(-1, fxLaunch, 0);
-}
