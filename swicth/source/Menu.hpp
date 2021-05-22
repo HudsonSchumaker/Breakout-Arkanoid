c5923832254c1993eb70539566b71a0b361047a6
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-23 00:54:41 +0200

    NS.menu changes

---
+++ swicth/source/Menu.hpp
@@ -0,0 +1,25 @@
+//
+// Switch
+// Menu.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+class Menu final {
+    public: 
+        
+        Menu();
+        ~Menu();
+
+    private:
+        bool start;
+        void load();
+        void input();
+        void loop();
+        void draw(SDL_Renderer* renderer);
+        void exit();
+};
