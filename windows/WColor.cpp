8492d6be662fd526b3beb247b7407f202f06827f
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-25 20:03:13 +0200

    Win  - WColor.cpp

---
+++ windows/WColor.cpp
@@ -0,0 +1,47 @@
+//
+// NGC
+// Color.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "WColor.hpp"
+
+WColor::WColor() : r(255), g(255), b(255), a(255) {}
+WColor::WColor(int r, int g, int b, int a) : r(r), g(g), b(b), a(a) {}
+
+Vector4 WColor::getRed() {
+    return Vector4 { 255, 0, 0, 255 };
+}
+
+Vector4 WColor::getGreen() {
+    return Vector4 { 0, 255, 0, 255 };
+}
+
+Vector4 WColor::getBlue() {
+    return Vector4 { 0, 0, 255, 255 };
+}
+
+Vector4 WColor::getWhite() {
+    return Vector4 { 255, 255, 255, 255 };
+}
+
+Color WColor::getBlack() {
+    return Color { 0, 0, 0, 255 };
+}
+
+Vector4 WColor::getGray() {
+    return Vector4 { 128, 128, 128, 255 };
+}
+
+Color WColor::getOrange() {
+    return Color { 255, 200, 0, 255 };
+}
+
+Vector4 WColor::getYellow() {
+    return Vector4 { 255, 255, 0, 255 };
+}
+
+Vector4 WColor::getPink() {
+    return Vector4 { 255, 175, 175, 255 };
+}
