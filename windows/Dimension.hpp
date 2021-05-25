a232f49e1db633df90ad408a7adf707a6b785d59
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-25 20:15:49 +0200

    Win - add Dim hpp

---
+++ windows/Dimension.hpp
@@ -0,0 +1,26 @@
+//
+// NGC
+// Dimension.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+
+class Dimension final {
+	private:
+		int w, h;
+
+	public:
+		Dimension();
+		Dimension(int w, int h);
+
+		void setDimension(Dimension d);
+		Dimension getSize();
+
+		void setWidth(int w) { this->w = w; };
+		int getWidth() const { return w; };
+
+		void setHeight(int h) { this->h = h; };
+		int getHeight() const { return h; };
+};
