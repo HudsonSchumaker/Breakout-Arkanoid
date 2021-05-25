a22695372567624d68076b91273d73b58d72a10b
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-25 20:03:34 +0200

    Win - WColor.hpp

---
+++ windows/WColor.hpp
@@ -0,0 +1,25 @@
+//
+// NGC
+// Color.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "raylib.h"
+
+class WColor final {
+    public:
+        int r, g, b, a;
+        WColor();
+        WColor(int r, int g, int b, int a);
+        static Vector4 getRed();
+        static Vector4 getGreen();
+        static Vector4 getBlue();
+        static Vector4 getWhite();
+        static Color getBlack();
+        static Vector4 getGray();
+        static Color getOrange();
+        static Vector4 getYellow();
+        static Vector4 getPink();
+};
