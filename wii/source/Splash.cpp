23b1c633e328fe3aa9c1c71ef5498ee86efed27a
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-26 18:37:26 +0200

    Wii - add Splash.

---
+++ wii/source/Splash.cpp
@@ -0,0 +1,37 @@
+//
+// Wii
+// Splash.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Splash.hpp"
+#include "Color.hpp"
+#include "logo_png.h"
+
+Splash::Splash() {
+    load();
+    loop();
+}
+
+Splash::~Splash() {
+    exit();
+}
+
+void Splash::load() {
+    logo = GRRLIB_LoadTexture(logo_png);
+}
+
+void Splash::loop() {
+    while (splashTimer < 120) {
+        GRRLIB_FillScreen(Color::getWhite()); 
+        GRRLIB_DrawImg(100, 180, logo, 0, 1, 1, Color::getWhite());
+        GRRLIB_Render();
+        splashTimer++;
+    }
+    exit();
+}
+
+void Splash::exit() {
+    GRRLIB_FreeTexture(logo);
+}
---
+++ wii/source/Splash.hpp
@@ -0,0 +1,23 @@
+//
+// Wii
+// Splash.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include <grrlib.h>
+
+class Splash final {
+    public: 
+        GRRLIB_texImg* logo;
+        
+        Splash();
+        ~Splash();
+
+    private:
+        double splashTimer;
+        void load();
+        void loop();
+        void exit();
+};
