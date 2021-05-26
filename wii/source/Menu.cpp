a3b1e03b10849b0f865f3cb22a87fa929e35d6e0
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-26 18:37:05 +0200

    Wii - add Menu

---
+++ wii/source/Menu.cpp
@@ -0,0 +1,57 @@
+//
+// Wii
+// Menu.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include <ctime>
+#include "Menu.hpp"
+#include "Color.hpp"
+#include "Canvas.hpp"
+#include "font_ttf.h"
+
+Menu::Menu() {
+    start = false;
+    load();
+    loop();
+}
+
+Menu::~Menu() {
+    exit();
+}
+
+void Menu::load() {
+    font = GRRLIB_LoadTTF(font_ttf, font_ttf_size);
+}
+
+void Menu::input() {
+    PAD_ScanPads();
+    int buttonsDown = PAD_ButtonsDown(0);
+		
+	if(buttonsDown & PAD_BUTTON_A) {
+		start = true;
+	}
+}
+
+void Menu::loop() {
+    while (!start) {
+        input();
+       
+        GRRLIB_FillScreen(Color::getBlack()); 
+        GRRLIB_PrintfTTF(Canvas::screenWidth/2 - 150, Canvas::screenHeight/2 - 158, font, "Breakanoid", 72, Color::getOrange());
+        
+        time_t currentTime;
+	    time(&currentTime);
+        if (currentTime % 2 == 0) {
+            GRRLIB_PrintfTTF(Canvas::screenWidth/2 - 66, Canvas::screenHeight/2 + 64, font, "- Press A to start -", 16, Color::getGray());
+        }
+        
+        GRRLIB_Render();
+    }
+    exit();
+}
+
+void Menu::exit() {
+    GRRLIB_FreeTTF(font);
+}
---
+++ wii/source/Menu.hpp
@@ -0,0 +1,24 @@
+//
+// Wii
+// Menu.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include <grrlib.h>
+
+class Menu final {
+    public: 
+        GRRLIB_ttfFont *font;
+        
+        Menu();
+        ~Menu();
+
+    private:
+        bool start;
+        void load();
+        void input();
+        void loop();
+        void exit();
+};
