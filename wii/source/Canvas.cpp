9fa2b34b2c6e10dbad2d2b959aa47c7c610ca0cc
Hudson Schumaker <hudson.schumaker@me.com>
2021-05-26 18:30:00 +0200

    Wii - add Canvas

---
+++ wii/source/Canvas.cpp
@@ -0,0 +1,11 @@
+//
+// Wii 
+// Canvas.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Canvas.hpp"
+
+Canvas::Canvas() : w(0), h(0) {}
+Canvas::Canvas(int w, int h) : w(w), h(h) {}
