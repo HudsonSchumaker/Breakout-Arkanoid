184da573c91730b6e3a313baf72d634abb9024a6
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-02 00:04:15 +0200

    Wii add Sprite

---
+++ wii/source/Sprite.cpp
@@ -0,0 +1,16 @@
+// 
+// Wii
+// Sprite.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Sprite.hpp"
+
+Sprite::Sprite() {}
+Sprite::Sprite(int x, int y) : x(x), y(y) {}
+Sprite::Sprite(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
+
+Rectangle Sprite::getBounds() {
+	return Rectangle(this->x, this->y, this->w, this->h);
+}
