85fa1872ccae79ec361c2bfd21dd591dad8ddee2
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-02 00:02:27 +0200

    Wii add Point

---
+++ wii/source/Point.hpp
@@ -0,0 +1,27 @@
+//
+// Wii
+// Point.hpp
+// SchumakerTam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+
+class Point final {
+    private:
+        int x, y;
+    
+    public:
+        Point();
+        Point(int x, int y);
+    
+        void move(int x, int y);
+	    void translate(int d);
+        void translate(int dx, int dy);
+    
+        void setX(int x) { this->x = x; };
+        int getX() const { return x; };
+    
+        void setY(int y) { this->y = y; };
+        int getY() const { return y; };
+};
