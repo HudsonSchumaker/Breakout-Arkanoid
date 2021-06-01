7e66b9a11d60825d5024c9887fcae7ea449d607d
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-01 23:33:48 +0200

    Wii - add Ball

---
+++ wii/source/Ball.cpp
@@ -0,0 +1,37 @@
+//
+// Wii
+// Ball.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Ball.hpp"
+#include "Color.hpp"
+
+void Ball::move() {
+    x += dx * s;
+    y += dy * s;
+    
+    if (x <= 128) { setDX(1); }
+    if (x >= 501) { setDX(-1); }
+    if (y <= 0) { setDY(1); }
+}
+
+void Ball::draw() {
+    GRRLIB_DrawImg(x, y, sprite, 0, 1, 1, Color::getWhite());
+}
+
+void Ball::draw(float d) {
+    GRRLIB_DrawImg(x, y, sprite, d, 1, 1, Color::getWhite());
+}
+
+/*
+    GRRLIB  GRRLIB_DrawImg method params
+    xpos	Specifies the x-coordinate of the upper-left corner.
+    ypos	Specifies the y-coordinate of the upper-left corner.
+    tex	    The texture to draw.
+    degrees	Angle of rotation.
+    scaleX	Specifies the x-coordinate scale. -1 could be used for flipping the texture horizontally.
+    scaleY	Specifies the y-coordinate scale. -1 could be used for flipping the texture vertically.
+    color	Color in RGBA format.
+*/
---
+++ wii/source/Ball.hpp
@@ -0,0 +1,31 @@
+//
+// Wii
+// Ball.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "Sprite.hpp"
+
+class Ball final: public Sprite {
+    public:
+        Ball() : Sprite(), dx(1), dy(-1), s(1) {};
+        Ball(int x, int y) : Sprite(x, y), dx(1), dy(-1), s(1) {};
+        Ball(int x, int y, int w, int h) : Sprite(x, y, w, h), dx(1), dy(-1), s(1) {};
+    
+        void move();
+        void reset(int x, int y);
+        void draw() override;
+        void draw(float d);
+
+        void setSpeed(int s) { this->s = s; }; 
+        int getDX() const { return dx; };
+	    void setDX(int dx) { this->dx = dx; };
+        int getDY() const { return dy; };
+        void setDY(int dy) { this->dy = dy; };
+
+    private:
+        int dx, dy;
+        int s;
+};
