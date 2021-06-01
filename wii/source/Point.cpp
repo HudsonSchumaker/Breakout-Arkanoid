3bd4b57117c01f265889f8122c88612ae412f742
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-02 00:02:18 +0200

    Wii add Point

---
+++ wii/source/Point.cpp
@@ -0,0 +1,26 @@
+//
+// Wii
+// Point.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Point.hpp"
+
+Point::Point() : x(0), y(0) {}
+Point::Point(int x, int y) : x(x), y(y) {}
+
+void Point::move(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
+
+void Point::translate(int d) {
+	this->x += d;
+	this->y += d;
+}
+
+void Point::translate(int dx, int dy) {
+    this->x += dx;
+    this->y += dy;
+}
