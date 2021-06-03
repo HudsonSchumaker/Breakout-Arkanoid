41f3aea8b353c2bf711b89d2455f172ad1986cd8
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-03 21:21:06 +0200

    NGC - Add Save.

---
+++ gamecube/source/Save.cpp
@@ -0,0 +1,18 @@
+//
+// NGC
+// Save.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Save.hpp"
+
+Save::Save() {
+    fatInitDefault();
+}
+
+Save::~Save() {}
+
+void Save::save(int level) {
+
+}
