af5aed9a33199e49d9c6c198a1110e82e93cf8a1
Hudson Schumaker <hudson.schumaker@me>
2021-06-06 00:57:14 +0200

    MacOS -  Add Files

---
+++ macos/Splash.cpp
@@ -0,0 +1,38 @@
+//
+// Win
+// Splash.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Splash.hpp"
+#include "Canvas.hpp"
+#include "WColor.hpp"
+
+Splash::Splash() {
+    load();
+    loop();
+}
+
+Splash::~Splash() {
+    exit();
+}
+
+void Splash::load() {
+    logo = LoadTexture("resources/logo.png");
+}
+
+void Splash::loop() {
+    while (splashTimer < 120) {
+        BeginDrawing();
+            ClearBackground(WColor::getWhite());
+            DrawTexture(logo, Canvas::screenWidth/2 - logo.width/2, Canvas::screenHeight/2 - logo.height/2, WColor::getWhite());         
+        EndDrawing();
+        splashTimer++;
+    }
+    exit();
+}
+
+void Splash::exit() {
+    UnloadTexture(logo);
+}
---
+++ macos/Splash.hpp
@@ -0,0 +1,23 @@
+//
+// Win
+// Splash.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "raylib.h"
+
+class Splash {
+    public: 
+        Texture2D logo;
+        
+        Splash();
+        ~Splash();
+
+    private:
+        double splashTimer;
+        void load();
+        void loop();
+        void exit();
+};
