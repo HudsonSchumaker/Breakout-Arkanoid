9325cda91c65f6bdf9c589819900d28881940c17
Hudson Schumaker <hudson.schumaker@me>
2021-06-06 12:18:22 +0200

    MacOS - add

---
+++ macos/WColor.hpp
@@ -0,0 +1,25 @@
+//
+// Win
+// Color.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "raylib.h"
+
+class WColor {
+    public:
+        int r, g, b, a;
+        WColor();
+        WColor(int r, int g, int b, int a);
+        static Color getRed();
+        static Color getGreen();
+        static Color getBlue();
+        static Color getWhite();
+        static Color getBlack();
+        static Color getGray();
+        static Color getOrange();
+        static Color getYellow();
+        static Color getPink();
+};
