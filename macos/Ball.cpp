78e3fae2b73b3caaf3c38705769abcf42c402399
Hudson Schumaker <hudson.schumaker@me>
2021-06-06 12:12:33 +0200

    MacOS - Add Ball

---
+++ macos/Ball.cpp
@@ -0,0 +1,30 @@
+//
+// macOS
+// Ball.cpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#include "Ball.hpp"
+#include "WColor.hpp"
+
+void Ball::move() {
+    x += dx * s;
+    y += dy * s;
+    
+    if (x <= 128) { setDX(1); }
+    if (x >= 501) { setDX(-1);}
+    if (y <= 0)   { setDY(1); }
+}
+
+void Ball::draw() {
+    DrawTexture(sprite, x, y, WColor::getWhite());             
+}
+
+void Ball::draw(float d) {
+    float fx = x;
+    float fy = y;
+    Vector2 pos = {fx, fy};
+    DrawTextureEx(sprite, pos, d, 1.0f, WColor::getWhite()); 
+}
+
