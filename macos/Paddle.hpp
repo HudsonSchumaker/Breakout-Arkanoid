5a12a537dcc84d3f840e7d1157ffca5c2a21d318
Hudson Schumaker <hudson.schumaker@me>
2021-06-06 12:15:59 +0200

    MacOS - add Paddle

---
+++ macos/Paddle.hpp
@@ -0,0 +1,19 @@
+//
+// Win
+// Paddle.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "Sprite.hpp"
+
+class Paddle : public Sprite {
+    public:
+        Paddle() : Sprite() {};
+        Paddle(int x, int y) : Sprite(x, y) {};
+        Paddle(int x, int y, int w, int h) : Sprite(x, y, w, h) {};
+        void move(int dx);
+        void draw() override;
+        void draw(float d);
+};
