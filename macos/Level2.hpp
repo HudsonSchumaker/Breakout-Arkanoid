cd4bf6cb6f4e616e51c11afcf6cf6d9996f093ce
HudsonSchumaker <hudson.schumaker@me.com>
2021-07-14 19:13:42 +0200

    MacOS - Lvl2

---
+++ macos/Level2.hpp
@@ -0,0 +1,45 @@
+//
+// macOS
+// Level2.hpp
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "Canvas.hpp"
+#include "Brick.hpp"
+#include "Ball.hpp"
+#include "Paddle.hpp"
+#include "raylib.h"
+
+class Level2 : public Canvas {
+    public:
+        const static int NUMBER_BRICK = 24;
+        
+        Level2();
+        ~Level2();
+
+        bool loop() override;
+        void input() override; 
+        void move() override;
+        void collision() override;
+        void render() override;
+        void load() override;
+        void unload() override;
+
+    private:
+        bool levelOver = false;
+        bool levelWon = false;
+
+        Paddle paddle;
+        Brick bricks[NUMBER_BRICK];    
+        Ball ball;
+
+        Texture2D red_brick_img;
+        Texture2D green_brick_img;
+        Texture2D paddle_img;
+        Texture2D ball_img;
+        Texture2D back_img;        
+        Font font;
+        Sound beep;
+};
