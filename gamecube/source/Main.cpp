c436b4461339b9a3d163f81daafab3f41072bc51
HudsonSchumaker <hudson.schumaker@me.com>
2022-03-31 20:53:14 +0200

    Test compile apple sillicon.

--- gamecube/source/Main.cpp
+++ gamecube/source/Main.cpp
@@ -27,51 +27,16 @@ int main(void) {
     ini();
 
     int level = 0;
-    ReadWrite rw = ReadWrite();
-    level = rw.load();
-    
+
     Splash splash = Splash();
     Menu menu = Menu(level);
     
     Level1* lv1 = new Level1();
     while(!lv1->loop()) {
-        delete lv1;
         lv1 = new Level1();
     }
     delete lv1;
-    rw.save(1);
-    
-    Level2* lv2 = new Level2();
-    while(!lv2->loop()) {
-        delete lv2;
-        lv2 = new Level2();
-    }
-    delete lv2;
-    rw.save(2);
-
-    Level3* lv3 = new Level3();
-    while(!lv3->loop()) {
-        delete lv3;
-        lv3 = new Level3();
-    }
-    delete lv3;
-    rw.save(3);
-
-    Level4* lv4 = new Level4();
-    while(!lv4->loop()) {
-        delete lv4;
-        lv4 = new Level4();
-    }
-    delete lv4;
-    rw.save(4);
-
-    Level5* lv5 = new Level5();
-    while(!lv5->loop()) {
-        delete lv5;
-        lv5 = new Level5();
-    }
-    delete lv5;
-    rw.save(5);
+   
 
     end();
     return 0;
